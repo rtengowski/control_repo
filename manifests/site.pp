20210e0a03303921bf09f79dde60747c980de772
rtengowski <99032083+rtengowski@users.noreply.github.com>
2022-02-07 15:30:36 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,5 @@
-node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
-  file {'/root/TEST':
-    owner   => 'root',
-    content => 'testing testing testing
-    ',
-    ensure  => file,
-  }
+node default {  
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
