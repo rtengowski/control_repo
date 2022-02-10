56e5fbc48ebe8805add2df963425b434491088e7
rtengowski <99032083+rtengowski@users.noreply.github.com>
2022-02-10 14:03:03 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -21,5 +21,9 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
-    
+  
+  service{'minecraft':
+    ensure => running,
+    enable => true,  
+  }
 }
