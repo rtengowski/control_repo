a24bc9d8dc117a4a443a3ea672c6d5c9bf34400e
rtengowski <99032083+rtengowski@users.noreply.github.com>
2022-03-07 16:02:37 -0500

    Update init.pp

--- site/test/manifests/init.pp
+++ site/test/manifests/init.pp
@@ -7,4 +7,10 @@ class test {
     ensure => file,
     content => 'this is a test',
   }
+  
+  vcsrepo { '/opt/test/repo':
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/rtengowski/test_repo.git',
+  }
 }
